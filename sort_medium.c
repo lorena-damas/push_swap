69fb04699c0f5a86fbb3f7496860152cf6049819
jotto <jotto@student.42berlin.de>
2026-07-26 20:25:29 +0200

    removed included but unnecessary library (stdlib still needs to be removed from sorting_helpers and exchanged with ftprintf

--- sort_medium.c
+++ sort_medium.c
@@ -6,12 +6,11 @@
 /*   By: jotto <jotto@student.42berlin.de>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/25 14:41:35 by jotto             #+#    #+#             */
-/*   Updated: 2026/07/26 20:12:15 by jotto            ###   ########.fr       */
+/*   Updated: 2026/07/26 20:17:51 by jotto            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
-#include <stdlib.h>
 
 void	sort_medium(int *a, int *b, int *sizea, int *sizeb)
 {
