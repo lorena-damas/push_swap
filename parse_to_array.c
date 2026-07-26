62cfcef0913482fce5cbee1e63a7a9c71fa63c5c
jotto <jotto@student.42berlin.de>
2026-07-26 15:40:51 +0200

    fixed the get_strategy ( "\0"-Terminator was off by 1 char)

--- parse_to_array.c
+++ parse_to_array.c
@@ -3,13 +3,13 @@
 
 static int	get_strategy(char *arg, t_strategy *strategy)
 {
-	if (ft_strncmp(arg, "--simple", 9) == 0 && arg[9] == '\0')
+	if (ft_strncmp(arg, "--simple", 9) == 0 && arg[8] == '\0')
 		*strategy = SIMPLE;
-	else if (ft_strncmp(arg, "--medium", 9) == 0 && arg[9] == '\0')
+	else if (ft_strncmp(arg, "--medium", 9) == 0 && arg[8] == '\0')
 		*strategy = MEDIUM;
-	else if (ft_strncmp(arg, "--complex", 10) == 0 && arg[10] == '\0')
+	else if (ft_strncmp(arg, "--complex", 10) == 0 && arg[9] == '\0')
 		*strategy = COMPLEX;
-	else if (ft_strncmp(arg, "--adaptive", 11) == 0 && arg[11] == '\0')
+	else if (ft_strncmp(arg, "--adaptive", 11) == 0 && arg[10] == '\0')
 		*strategy = ADAPTIVE;
 	else
 		return (0);
