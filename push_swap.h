0b8ab3f3efbd3f597aa2f5fb84761239e0fef8b3
Jonas <i-otto@protonmail.com>
2026-07-26 12:28:04 +0200

    Add strategy functions to push_swap.h

--- push_swap.h
+++ push_swap.h
@@ -13,6 +13,24 @@
 #ifndef PUSH_SWAP_H
 # define PUSH_SWAP_H
 
+typedef enum e_strategy
+{
+	SIMPLE,
+	MEDIUM,
+	COMPLEX,
+	ADAPTIVE
+} t_strategy;
+
 int	check_input(int argc, char **argv, int *values);
+int	parse_args(int argc, char **argv, int *values, t_strategy *strategy);
+void	sort_simple(int *a, int *b, int *na, int *nb);
+void	sort_medium(int *a, int *b, int *na, int *nb);
+void	sort_complex(int *a, int *b, int *na, int *nb);
+void	adaptive_sort(int *a, int *b, int *na, int *nb);
+int	find_min_value_index(int *values, int count);
+int	find_max_value_index(int *values, int count);
+int	*sorted_copy(int *values, int count);
+void	sort_array(int *values, int count);
+int	get_rank(int *sorted, int count, int value);
 
 #endif
